f5a56490efa370596860418d212e5a1bc204b568
Shukla <vineet.shukla@mind.motherson.com>
2023-01-09 13:21:56 +0530

    chnages

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+$class = new \Vineet\MathmaticalOperation\Calculation();
+$result=$class->add(10,20);
+echo $result;
+
+
---
+++ src/Calculation.php
@@ -0,0 +1,19 @@
+<?php
+namespace Vineet\MathmaticalOperation;
+class Calculation {
+    function __construct(){        
+    }
+    function add($a,$b){
+        return $a+$b;
+    }
+    function subtract($a,$b){
+        return $a+$b;
+    }
+    function multiply($a,$b){
+        return $a+$b;
+    }
+
+    function division($a,$b){
+        return $a/$b;
+    }
+}
